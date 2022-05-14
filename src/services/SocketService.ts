75b95b2228e238cc7bed87be7fb643f62e10f3c1
Vihaan <vverma270705@gmail.com>
2022-05-14 09:36:42 +0530

    Client side completed

--- src/services/SocketService.ts
+++ src/services/SocketService.ts
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 class SocketService {
   private socket: any;
   constructor() {
-    this.socket = io("http://localhost:5000");
+    this.socket = io(process.env.SERVER_URL || "http://localhost:5000");
   }
   public subscribeTo(eventName: string, callback: any) {
     this.socket.on(eventName, callback);
